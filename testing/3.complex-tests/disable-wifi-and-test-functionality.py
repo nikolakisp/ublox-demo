67cb3b8b3efdc680bbf1b9b01e1aeddc6ae043bb
StefaniaKappa <stefania@spannerci.com>
2018-11-21 16:27:03 +0200

    Update disable-wifi-and-test-functionality.py

--- testing/3.complex-tests/disable-wifi-and-test-functionality.py
+++ testing/3.complex-tests/disable-wifi-and-test-functionality.py
@@ -24,7 +24,7 @@ import sys
 from Spanner import Spanner
 from Testboard import Testboard
 
-testboard = Testboard("testboard_name")
+testboard = Testboard("Wubby_Test")
 
 # Our device's Output Pin will be connected to the Testboard's D7, making it our
 # Input Pin
