717d138f7f22230a3d62e35560b43785a0ab9a7a
StefaniaKappa <stefania@spannerci.com>
2019-01-15 15:57:43 +0200

    Update led.py

--- testing/led.py
+++ testing/led.py
@@ -1,15 +1,17 @@
 import gpio
+
 class LED:
-    def __init__(self, pinid, onLevel):
-        self.onLevel = onLevel
-        self.pinid = pinid
-        self.gpio = gpio.gpio(pinid, gpio.GPIO_MODE_OUTPUT_PULLDOWN)
-        self.gpio.gpioWrite(1 - self.onLevel)
 
-    def ON(self):
-        print("LED ON\r\n")
-        self.gpio.gpioWrite(self.onLevel)
+        def __init__(self, pinid, onLevel):
+                self.onLevel = onLevel
+                self.pinid = pinid
+                self.gpio = gpio.gpio(pinid, gpio.GPIO_MODE_OUTPUT_PULLDOWN)
+                self.gpio.gpioWrite(1 - self.onLevel)
 
-    def OFF(self):
-        print("LED OFF\r\n")
-        self.gpio.gpioWrite(1 - self.onLevel)
+        def ON(self):
+                print("LED ON\r\n")
+                self.gpio.gpioWrite(self.onLevel)
+
+        def OFF(self):
+                print("LED OFF\r\n")
+                self.gpio.gpioWrite(1 - self.onLevel)
