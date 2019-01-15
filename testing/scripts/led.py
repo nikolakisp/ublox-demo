7ba76dee98d6d8f98dfb84859f31cd4f31cdf438
StefaniaKappa <stefania@spannerci.com>
2019-01-15 16:10:37 +0200

    Update led.py

--- testing/scripts/led.py
+++ testing/scripts/led.py
@@ -5,7 +5,7 @@ class LED:
         def __init__(self, pinid, onLevel):
                 self.onLevel = onLevel
                 self.pinid = pinid
-                self.gpio = gpio.gpio(pinid, gpio.GPIO_MODE_OUTPUT_PULLDOWN)
+                self.gpio = gpio(pinid, gpio.GPIO_MODE_OUTPUT_PULLDOWN)
                 self.gpio.gpioWrite(1 - self.onLevel)
 
         def ON(self):
