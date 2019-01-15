700c60ca029d5ca5dfb4598b1ebe144035cf8d83
StefaniaKappa <stefania@spannerci.com>
2019-01-15 14:25:20 +0200

    Update scenario.py

--- testing/scenario.py
+++ testing/scenario.py
@@ -21,16 +21,12 @@ def button_release():
 if __name__ == "__main__":
     start_time = time.time()%60     #time start to seconds
     while 1:
-        #button_press()             #button is pressed
-        #time.sleep(2)
-        #led_on()                    #after 2 seconds led is on
-        #time.sleep(4)
+        button_press()             #button is pressed
+        time.sleep(4)
         button_release()            #after 4 second button is released and led is off
-        #time.sleep(4)
+        time.sleep(4)
         end_time = time.time()%60   
         stop = int(end_time - start_time)
         print (stop)
-#         if stop == 20 :             #after 60 second stop 
-#             break
-        break
-    print("blue")
+         if stop >= 20 :             #after 60 second stop 
+             break
