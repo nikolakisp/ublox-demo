9bf42a72bbf8db4c391269b0f4c81d795a7d69c2
StefaniaKappa <stefania@spannerci.com>
2019-01-15 17:18:41 +0200

    Update scenario.py

--- testing/scenario.py
+++ testing/scenario.py
@@ -7,7 +7,6 @@ testboard = Testboard("myd")
 
 OUTPUT_PIN = "D5"   #input of device => output pin of testboard
 INPUT_PIN = "D7"    #led of device => input pin of testboard
-
 def button_press():
     # check PIN state
     status = testboard.digitalWrite(OUTPUT_PIN,'LOW')
@@ -19,16 +18,19 @@ def button_release():
     status = testboard.digitalWrite(OUTPUT_PIN,'HIGH')
     Spanner.assertEqual(status,1)
 
-def led_on():
+def led_on(is_on):
     # check PIN state
     value = testboard.digitalRead(INPUT_PIN)
+    spanner.assertEqual(value,is_on)
 
 if __name__ == "__main__":
     start_time = datetime.now()     #time start to seconds
     while 1:
         button_press()             #button is pressed -> led is on
+        len_on(1)
         time.sleep(3)
         button_release()            #after 3 second button is released -> led is off
+        len_on(0)
         time.sleep(3)
         end_time = datetime.now()   
         stop = end_time - start_time
