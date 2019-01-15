d1b39bc5bbb49ef9f4082531586db6476e96a6f7
StefaniaKappa <stefania@spannerci.com>
2019-01-15 16:20:22 +0200

    Update main.py

--- testing/scripts/main.py
+++ testing/scripts/main.py
@@ -1,1 +1,16 @@
-print("hello")
+import time
+from hardware import gpio
+import led
+
+## Create a new led object using the pin 11
+led1 = led.LED(11,1)
+
+while True:
+    # Change LED state
+    led1.ON()
+
+    time.sleep(4000)
+
+    led1.OFF()
+
+    time.sleep(1000)
