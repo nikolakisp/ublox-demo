b974096ffdc2de7de1f1db01a14ddaea87401cdb
StefaniaKappa <stefania@spannerci.com>
2019-01-15 15:58:16 +0200

    Update main.py

--- testing/scripts/main.py
+++ testing/scripts/main.py
@@ -1,13 +1,19 @@
 # GPIOs Example - Showcase the GPIO module
 import time
 from hardware import gpio
+import led
+
 ## Create a new led object using the pin 11
-# wait for Testboard's input
-pin1 = gpio(2, gpio.GPIO_MODE_INPUT_PULLUP)
-while 1:
-    if str(pin1.read()) == "1":
-        gpio.gpioWrite(12,1)    # led
+led1 = led.LED(11,1)
+
+# Set GPIO 2 as input
+sw2 = gpio(2, gpio.GPIO_MODE_INPUT_PULLUP)
+
+while True:
+    # Toggle Led based on SW2 Button
+    if (str(sw2.read()) == "1"):
+        led1.OFF()
         time.sleep(3000)
-    else :
-        gpio.gpioWrite(12,0)
-        time.sleep(1000)
+    else:
+        led1.ON()
+        time.sleep(3000)
