00d067b9fbe84c4cf19267bcde49f24cd7818337
StefaniaKappa <stefania@spannerci.com>
2019-01-16 10:57:20 +0200

    Update scenario.py

--- testing/scenario.py
+++ testing/scenario.py
@@ -2,31 +2,42 @@ import time
 import Spanner
 from Testboard import Testboard
 
-testboard = Testboard("myd")
+testboard = Testboard("testboard_name")
+# Our device's Input (GPIO2 - SW2) will be connected the Testboard's Pin D5, making it our
+# Output Pin
+OUTPUT_PIN = "D5"
 
-OUTPUT_PIN = "D5"   #input of device => output pin of testboard
-INPUT_PIN = "D6"    #led of device => input pin of testboard
+# Our device's Output Pin (GPIO11 - Led D1) will be connected to the Testboard's D6, making it
+# our Input Pin
+INPUT_PIN = "D6"
 def button_press():
     # check PIN state
     status = testboard.digitalWrite(OUTPUT_PIN,'LOW')
     Spanner.assertEqual(status,1)
-    
-    
+
 def button_release():
     # check PIN state
     status = testboard.digitalWrite(OUTPUT_PIN,'HIGH')
     Spanner.assertEqual(status,1)
 
-def led(value):
+def is_led_on():
     # check PIN state
     status = testboard.digitalRead(INPUT_PIN)
-    Spanner.assertEqual(status,value)
+    Spanner.assertEqual(status,1)
+
+def is_led_off():
+    # check PIN state
+    status = testboard.digitalRead(INPUT_PIN)
+    Spanner.assertEqual(status,0)
+
+def validate_led_on_button_press():
+    for i in range(10):
+        button_press()
+        is_led_on()
+        time.sleep(3)
+        button_release()
+        is_led_off()
+        time.sleep(3)
 
 if __name__ == "__main__":
-    for i in range(10):
-        button_press()             #button is pressed -> led is on
-        led(1)
-        time.sleep(3)
-        button_release()            #after 3 second button is released -> led is off
-        led(0)
-        time.sleep(3)
+    validate_led_on_button_press()
